8b5a1bd3ba21f857440842fac4953ca946daac5c
olegsmirnov <olegsmirnov2001@gmail.com>
2016-01-26 19:30:55 +0400

    First commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include "TXLib.h"
+#include "MyLib.h"
+
+int main ()
+    {
+    CreateMyWindow ();
+
+    int Sz1 = 0, Sz2 = 0;
+    scanf ("%d %d\n", &Sz1, &Sz2);
+
+    matrix_t <int> matr (5, 5);
+
+    for (int number1 = 0; number1 < Sz1; number1++)
+        {
+        for (int number2 = 0; number2 < Sz2; number2++)
+            scanf ("%d ", matr [number1][number2]);
+
+        scanf ("\n");
+        }
+
+    PrintfMatrix (matr);
+
+    //_txExit = true;
+    return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
