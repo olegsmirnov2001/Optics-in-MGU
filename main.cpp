43382d2a590a71c702a94d278e784433a846034c
olegsmirnov <olegsmirnov2001@gmail.com>
2016-01-30 16:55:37 +0400

    Testing

--- main.cpp
+++ main.cpp
@@ -1,14 +1,38 @@
-#include "TXLib.h"
 #include "MyLib.h"
+
+template <template <typename, int> class Type, int Sz>
+bool Main (array_t <Type, button_t, Sz> & buttons);
 
 int main ()
     {
     CreateMyWindow (Window, true);
 
     array_t <stt, button_t, 10> buttons;
+    SetButtons (buttons);
+
+    Main (buttons);
 
     _txExit = true;
     return 0;
+    }
+
+template <template <typename, int> class Type, int Sz>
+bool Main (array_t <Type, button_t, Sz> & buttons)
+    {
+    txBegin ();
+
+    while (true)
+        {
+        Clear ();
+
+        DoOperationsWithButtons (buttons);
+
+        txSleep (1);
+        }
+
+    txEnd ();
+
+    return true;
     }
 
 
